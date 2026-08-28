96e9810a5a7628aef9b1faf5726cb448c083ecb2
Peter Boothe <peter@triplepat.com>
2026-08-28 23:24:24 +0200

    Scaffold the module and toolchain

---
+++ doc.go
@@ -0,0 +1,3 @@
+// Package syntheticalert emits a synthetic alert measurement so you can prove,
+// continuously and in production, that your alerting pipeline works.
+package syntheticalert
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/triple-pat/go-syntheticalert
+
+go 1.25.0
